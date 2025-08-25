f5c59abe3f4d30ef89f1e938e7df4235918d30fb
Eduardo Corrales Iturbe <ecorrales@uninter.edu.mx>
2025-08-25 07:25:34 -0600

    Commit 2 cambios en mi laptop

--- ExplicacionGIT/Program.cs
+++ ExplicacionGIT/Program.cs
@@ -1,61 +1,100 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ExplicacionGIT
+class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        int opcion;
+
+        do
         {
+            Console.Clear();
+            Console.WriteLine("=== MENÚ DE OPCIONES ===");
+            Console.WriteLine("1. Comparar dos números");
+            Console.WriteLine("2. Tabla de multiplicar");
+            Console.WriteLine("3. Suma de números pares del 1 al 20");
+            Console.WriteLine("4. Salir");
+            Console.Write("Elige una opción: ");
+            opcion = Convert.ToInt32(Console.ReadLine());
 
-
-
-            //Console.WriteLine("numero1");
-            //int num1 = Convert.ToInt32(Console.ReadLine());
-            //Console.WriteLine("numero2");
-            //int num2 = Convert.ToInt32(Console.ReadLine());
-
-            //if (num1 > num2)
-            //{
-            //    Console.WriteLine($"Mayor {num1}");
-
-            //}
-            //else if (num1 < num2)
-            //{
-            //    Console.WriteLine($"mayor {num2}");
-            //}
-            //else
-            //{
-            //    Console.WriteLine("Son iguales");
-            //}
-
-
-            //Console.WriteLine("Dame la tabla de multiplicar");
-            //int tabla = Convert.ToInt32(Console.ReadLine());
-            //for (int i = 1; i <= 10; i++)
-            //{
-            //    Console.WriteLine($"{tabla} x {i}={tabla*i}");
-            //}
-            int num = 1;
-            int suma = 0;
-            while (num <= 20)
+            switch (opcion)
             {
-                if (num % 2 == 0)
-                {
-                    //ESTO ES PAR
-                    Console.WriteLine(num);
-                    //sumar numeros pares
-                    suma = suma + num;
-                }
-                num++;
+                case 1:
+                    CompararNumeros();
+                    break;
+                case 2:
+                    TablaMultiplicar();
+                    break;
+                case 3:
+                    SumaNumerosPares();
+                    break;
+                case 4:
+                    Console.WriteLine("Saliendo del programa...");
+                    break;
+                default:
+                    Console.WriteLine("Opción no válida.");
+                    break;
             }
-            Console.WriteLine($"la suma es:{suma}");
 
-            Console.ReadKey();
+            if (opcion != 4)
+            {
+                Console.WriteLine("\nPresiona cualquier tecla para continuar...");
+                Console.ReadKey();
+            }
+
+        } while (opcion != 4);
+    }
+
+    static void CompararNumeros()
+    {
+        Console.WriteLine("Introduce el primer número:");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Introduce el segundo número:");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        if (num1 > num2)
+        {
+            Console.WriteLine($"Mayor: {num1}");
+        }
+        else if (num1 < num2)
+        {
+            Console.WriteLine($"Mayor: {num2}");
+        }
+        else
+        {
+            Console.WriteLine("Son iguales");
         }
     }
-}
 
+    static void TablaMultiplicar()
+    {
+        Console.WriteLine("¿De qué número quieres la tabla de multiplicar?");
+        int tabla = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine($"{tabla} x {i} = {tabla * i}");
+        }
+    }
+
+    static void SumaNumerosPares()
+    {
+        int num = 1;
+        int suma = 0;
+
+        Console.WriteLine("Números pares del 1 al 20:");
+
+        while (num <= 20)
+        {
+            if (num % 2 == 0)
+            {
+                Console.WriteLine(num);
+                suma += num;
+            }
+            num++;
+        }
+
+        Console.WriteLine($"La suma de los números pares es: {suma}");
+    }
+}
